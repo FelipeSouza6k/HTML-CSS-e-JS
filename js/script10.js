0833a511ff20bbc8a787396186bf4583345cb6a5
Felipe Souza <125583116+FelipeSouza6k@users.noreply.github.com>
2025-04-22 12:28:18 -0300

    Update script10.js

--- js/script10.js
+++ js/script10.js
@@ -1,42 +1,23 @@
-function soma (p1, p2) {
-  return p1 + p2
+let p1 = parseFloat(prompt('Digite o primeiro valor: '));
+let p2 = parseFloat(prompt('Digite o segundo valor: '));
+alert("Agora clique na operação desejada");
+function soma (p1, p2) {  
+  resultado = p1 + p2;
+  document.getElementById('demo').innerHTML = resultado;
 }
 function subtracao (p1, p2) {
-  return p1 - p2
+  resultado = p1 - p2;
+  document.getElementById('demo').innerHTML = resultado;
 }
+
 function divisao (p1, p2) {
-  return p1 / p2
+  resultado = p1 / p2;
+  document.getElementById('demo').innerHTML = resultado;
+  if (p2 === 0){
+    alert("Não é possível fazer divisão com 0");
+  }
 }
 function multiplicacao (p1, p2) {
-  return p1 * p2
+  resultado = p1 * p2
+  document.getElementById('demo').innerHTML = resultado;
 }
-let operacao = prompt('Digite o sinal da operação desejada\nSoma = " + "\nSubtração = " - "\nMultiplicação = " * "\nDivisão = " / "');
-let p1 = parseFloat(prompt('Digite o primeiro valor: '))
-let p2 = parseFloat(prompt('Digite o segundo valor: '))
-switch (operacao) {
-    case '+':
-      resultado = soma (p1, p2)
-      document.getElementById('demo').innerHTML = resultado
-      break;
-  
-    case '-':
-      resultado = subtracao (p1, p2)
-      document.getElementById('demo').innerHTML = resultado
-      break;
-  
-    case '/':
-      resultado = divisao (p1, p2)
-      document.getElementById('demo').innerHTML = resultado
-      break;
-  
-    case '*':
-      resultado = multiplicacao (p1, p2)
-      document.getElementById('demo').innerHTML = resultado
-      break;
-  
-    default:
-      alert('Operação invalida')
-      break;
-  }
-
-
